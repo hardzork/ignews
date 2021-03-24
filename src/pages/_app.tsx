90c4882d67420a3af514e0c2ca5b1d326f77080f
Robinson Junior <robinson.juniorr@gmail.com>
2021-03-24 01:07:27 -0300

    Componente Sign In Button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Header } from "../components/Header";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header></Header>
+      <Header />
       <Component {...pageProps} />
     </>
   );
