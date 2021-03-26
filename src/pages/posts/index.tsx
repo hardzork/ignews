2140c4a001b5cb563ad8cd79b4da7a28e098c99e
Robinson Junior <robinson.juniorr@gmail.com>
2021-03-26 10:10:15 -0300

    Consumindo API do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -38,3 +41,14 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.Predicates.at("document.type", "post")],
+    { fetch: ["post.title", "post.content"], pageSize: 100 }
+  );
+  console.log(JSON.stringify(response, null, 2));
+
+  return { props: {} };
+};
